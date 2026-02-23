cc7c2da0e9fa87bf384f654dcca626b00c1ac8a5
babue0 <iandpcarvalho@gmail.com>
2026-02-23 14:34:34 -0300

    first commit

---
+++ src/main/java/com/br/rotaaprovacao/RotaAprovacaoApplication.java
@@ -0,0 +1,13 @@
+package com.br.rotaaprovacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaAprovacaoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RotaAprovacaoApplication.class, args);
+  }
+
+}
